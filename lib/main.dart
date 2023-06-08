cf96b6bbb282d14b9b151eb6e81b231f47f29519
Anand Ayyasamy <ananda.ayyasamy@aciworldwide.com>
2023-06-08 10:39:15 -0400

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart'
-    hide EmailAuthProvider, GoogleAuthProvider;
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:game_finder/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
-import 'constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
