fd17e489afff3f0025783659498c8e2cfc6f3543
Anand Ayyasamy <ananda.ayyasamy@aciworldwide.com>
2023-06-08 08:50:38 -0400

    updated

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
+import 'package:game_finder/constants.dart';
 
 import 'home.dart';
 
@@ -16,13 +17,32 @@ class AuthGate extends StatelessWidget {
           return SignInScreen(
             providerConfigs: const [
               EmailProviderConfiguration(),
+              GoogleProviderConfiguration(clientId: googleClientId),
+              FacebookProviderConfiguration(clientId: ''),
+              AppleProviderConfiguration(),
             ],
             headerBuilder: (context, constraints, shrinkOffset) {
               return Padding(
                 padding: const EdgeInsets.all(20),
                 child: AspectRatio(
                   aspectRatio: 1,
-                  child: Image.asset('flutterfire_300x.png'),
+                  //child: Image.asset('game-finder-logo.png'),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/game-finder-logo.png',
+                      width: 100,
+                      height: 100,
+                    ),
+                  ),
+                ),
+              );
+            },
+            footerBuilder: (context, action) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'By signing in, you agree to our terms and conditions.',
+                  style: TextStyle(color: Colors.grey),
                 ),
               );
             },
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,4 @@
+//const googleClientId ='406099696497-a12gakvts4epfk5pkio7dphc1anjiggc.apps.googleusercontent.com';
+
 const googleClientId =
-    '406099696497-a12gakvts4epfk5pkio7dphc1anjiggc.apps.googleusercontent.com';
+    '242312898594-7sonijci58vp4dv7jhhpd5196e0c47tr.apps.googleusercontent.com';
